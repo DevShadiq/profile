827252c44860e291674bcf68c273ea108b43cc20
DevShadiq <shadiq1297@gmail.com>
2023-10-08 01:08:31 +0600

    favicon

--- admin/header.php
+++ admin/header.php
@@ -3,32 +3,34 @@
 
 <head>
     <meta charset="utf-8" />
-	<meta content="" name="description"/>
-    <meta content="" name="author"/>
+    <meta content="" name="description" />
+    <meta content="" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title><?php if(isset($title)){echo $title;}?></title>
-	<link rel="shortcut icon" href="<?php echo $path;?>/img/favicon.ico" type="image/x-icon">
-	
+    <title><?php if (isset($title)) {
+                echo $title;
+            } ?></title>
+    <link rel="shortcut icon" href="<?php echo $path; ?>/img/favicon.ico" type="image/x-icon">
+
     <!-- GLOBAL MAINLY STYLES-->
-    <link href="<?php echo $path;?>/assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="<?php echo $path;?>/assets/css/select2.min.css" rel="stylesheet" />
-    <link href="<?php echo $path;?>/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="<?php echo $path;?>/assets/themify-icons/css/themify-icons.css" rel="stylesheet" />
-	
+    <link href="<?php echo $path; ?>/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?php echo $path; ?>/assets/css/select2.min.css" rel="stylesheet" />
+    <link href="<?php echo $path; ?>/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="<?php echo $path; ?>/assets/themify-icons/css/themify-icons.css" rel="stylesheet" />
+
     <!-- THEME STYLES-->
-    <link href="<?php echo $path;?>/assets/css/main.min.css" rel="stylesheet" />
-	<link href="<?php echo $path;?>/assets/css/parsley.css" type="text/css" rel="stylesheet">
+    <link href="<?php echo $path; ?>/assets/css/main.min.css" rel="stylesheet" />
+    <link href="<?php echo $path; ?>/assets/css/parsley.css" type="text/css" rel="stylesheet">
     <link href="https://fonts.maateen.me/kalpurush/font.css" rel="stylesheet">
 
 
-	<link rel="stylesheet" href="<?php echo $path;?>/assets/css/style.css">
-	<link rel="stylesheet" href="<?php echo $path;?>/assets/css/print.css" media="print">
-    <link rel="stylesheet" href="<?php echo $path;?>/assets/css/responsive.css">
-	 <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,900&display=swap" rel="stylesheet">
-	 
+    <link rel="stylesheet" href="<?php echo $path; ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo $path; ?>/assets/css/print.css" media="print">
+    <link rel="stylesheet" href="<?php echo $path; ?>/assets/css/responsive.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700,900&display=swap" rel="stylesheet">
+
     <!-- PAGE LEVEL STYLES-->
 </head>
+
 <body class="fixed-navbar">
     <div class="page-wrapper">
-       
